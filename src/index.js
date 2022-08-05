5d2766021210a82e3c1421acad5a9dc90fdccc00
Volodkevich Sergey <volodkevich777@gmail.com>
2022-08-05 15:06:11 +0300

    feat: allmost done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let string = str;
+    let pass = 0;
+    let key = false;
+    for (let i = 0; i < bracketsConfig.length; i++){
+      for ( let j = 0; j < (str.length/2); j++){
+        let openIndex = string.indexOf(bracketsConfig[i][0]);
+        let closeIndex = string.lastIndexOf(bracketsConfig[i][1]);
+        if ((openIndex < closeIndex) && ((string.substring(openIndex, (closeIndex+1)).length % 2) == 0)){
+          string = string.slice(0, openIndex) + string.slice(openIndex+1, closeIndex) + string.slice(closeIndex+1);
+          pass++;
+        }
+      }  
+    }
+    if (pass == (str.length/2)){
+      key = true;
+    }
+    return(key);
 }
